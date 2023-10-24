fb8ffe1b8f91f1cffdc8a0f33b9645b54c371091
JanOrdenaAJEI <jan.fernandez@enti.cat>
2023-10-24 14:23:59 +0200

    NUEVP

--- Assets/Scripts/menuprincipalhard1.cs
+++ Assets/Scripts/menuprincipalhard1.cs
@@ -6,9 +6,9 @@ using UnityEngine.SceneManagement;
 public class menuprincipalhard : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void PLAY()
+    public void PLAY2()
     {
-        Debug.Log("PLAY");
+        Debug.Log("PLAY2");
         SceneManager.LoadScene("hardplay");
 
     }
